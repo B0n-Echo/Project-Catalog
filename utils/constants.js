656b76e3ad3d2fa4eacb1d63ba6899bee771ffac
Nikhilesh Pandey <33226238+B0n-Echo@users.noreply.github.com>
2022-03-28 08:50:22 +0530

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -3,5 +3,5 @@ exports.OPEN_LIBRARY_URL_IDN = 'OPEN';
 
 
 exports.GOOGLE_BOOKS_URL= 'https://www.googleapis.com/books/v1/volumes?q=';
-exports.GOOGLE_API_KEY = 'AIzaSyCPHj5XycnFj9xia6xKqmXF3iChLxplLB4'; // can be added in .env file as a better approach
+exports.GOOGLE_API_KEY = 'AIzaSyCPHxxxxxxxxxxxxxxxxxxxxxxxLxplLB4'; // can be added in .env file as a better approach
 exports.GOOGLE_BOOKS_URL_IDN = 'GOOGLE';
