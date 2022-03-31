f4e70ac77d82673797fcebdb6ff311d3d8f15a8d
NikhileshP <nikhileshhpandey@gmail.com>
2022-03-31 09:44:05 +0530

    🏗️ Adding new service for quote Api

---
+++ services/quoteService.js
@@ -0,0 +1,30 @@
+const constants = require('../utils/constants');
+const request = require('request');
+const utilities = require('../utils/utilities');
+
+exports.getQuote = function (req) {
+    return new Promise((resolve, reject) => {
+
+          let options = {
+            method: 'GET',
+            url:constants.QUOTES_URL,                      //'https://api.quotable.io/random?',
+            params: {tags: 'technology,famous-quotes'},
+          };
+
+        //   request.get(options, function(err, response) {
+        //     if(err || response.statusCode == 400) { 
+        //         let error = err ? err : response.body
+        //         console.log("🚀 ~ file: quoteController.js ~ line 23 ~ request.get ~ error", error)
+        //         reject (createError(`error in fetching data: ${error}`));
+        //     }
+
+        //     let {author, content} = JSON.parse(response.body);
+        //     resolve( {author, content} );
+        //   })
+
+        resolve({ author: 'Nelson Mandela',
+        content:
+         'If you talk to a man in a language he understands, that goes to his head. If you talk to him in his language, that goes to his heart.' }
+      )
+    })
+}
--- utils/constants.js
+++ utils/constants.js
@@ -5,3 +5,5 @@ exports.OPEN_LIBRARY_URL_IDN = 'OPEN';
 exports.GOOGLE_BOOKS_URL= 'https://www.googleapis.com/books/v1/volumes?q=';
 exports.GOOGLE_API_KEY = 'AIzaSyCPHj5XycnFj9xia6xKqmXF3iChLxplLB4'; // can be added in .env file as a better approach
 exports.GOOGLE_BOOKS_URL_IDN = 'GOOGLE';
+
+exports.QUOTES_URL = 'https://api.quotable.io/random?';
